165da46b8eec458b09f4986500a3ee9ae83e57c2
jonathanhb <jonathan3141@gmail.com>
2023-01-23 17:08:58 -0800

    updated paths

--- test/test_bfs.py
+++ test/test_bfs.py
@@ -31,10 +31,10 @@ def test_bfs_traversal():
 
     bfs_correct = ['Steven Altschuler', '32036252', 'Lani Wu', '32042149', '31806696', '30727954', 'Hani Goodarzi', 'Luke Gilbert', 'Michael McManus', '33232663', '33483487', '31626775', '31540829', '32025019', '29700475', 'Charles Chiu', 'Martin Kampmann', 'Neil Risch', 'Nevan Krogan', 'Atul Butte', 'Michael Keiser', '33242416', '32790644', '34272374', '32353859', '30944313', '33765435', '31395880', 'Marina Sirota', '31486345']
 
-    gtest = graph.Graph("../data/tiny_network.adjlist")
+    gtest = graph.Graph("data/tiny_network.adjlist")
     bfs = gtest.bfs("Steven Altschuler")
 
-    assert exception_assertion(['', None], graph.Graph("../data/empty_network.adjlist").bfs, "graph has no nodes"), "unit test for traversal part 1 failed; did not detect empty input graph"
+    assert exception_assertion(['', None], graph.Graph("data/empty_network.adjlist").bfs, "graph has no nodes"), "unit test for traversal part 1 failed; did not detect empty input graph"
 
     absent_node = '0'
     assert exception_assertion([absent_node, None], gtest.bfs, f"starting node {absent_node} not found in graph"), f"unit test for traversal part 2 failed; did not detect nonexistent starting node {absent_node}"
@@ -58,10 +58,10 @@ def test_bfs():
 
     bfs_correct = ['Steven Altschuler', '32036252', 'Lani Wu', '32042149', 'Hani Goodarzi', '33232663', 'Charles Chiu']
 
-    gtest = graph.Graph("../data/tiny_network.adjlist")
+    gtest = graph.Graph("data/tiny_network.adjlist")
     bfs = gtest.bfs("Steven Altschuler", "Charles Chiu")
 
-    assert exception_assertion(['', None], graph.Graph("../data/empty_network.adjlist").bfs, "graph has no nodes"), "unit test for path-finding part 1 failed; did not detect empty input graph"
+    assert exception_assertion(['', None], graph.Graph("data/empty_network.adjlist").bfs, "graph has no nodes"), "unit test for path-finding part 1 failed; did not detect empty input graph"
 
     absent_node = '0'
     assert exception_assertion([absent_node, None], gtest.bfs, f"starting node {absent_node} not found in graph"), f"unit test for path-finding part 2.0 failed; did not detect nonexistent starting node {absent_node}"
@@ -70,7 +70,7 @@ def test_bfs():
     #beware that if there are multiple equally short shortest paths and graph.py is updated to find a different one, this will fail
     assert bfs_correct == bfs, "unit test for path-finding part 3 failed; did not correctly identify the shortest path"
 
-    gtest2 = graph.Graph("../data/disconnected_network.adjlist")
+    gtest2 = graph.Graph("data/disconnected_network.adjlist")
     bfs2 = gtest2.bfs("Steven Altschuler", "Charles Chiu")
 
     assert bfs2 == None, "unit test for path-finding part 4 failed; did not recognize unreachable end node"
